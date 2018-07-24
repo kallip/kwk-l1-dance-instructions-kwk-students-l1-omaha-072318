93ce094fe70160880d2c80d1e70c2dbe1939d99d
kallip <kallinpettit@gmail.com>
2018-07-24 00:12:28 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,14 @@
 
 # Code your methods below
+def starting_stance 
+  puts "Plant legs far apart"
+end
+
+def skip_step(left)
+  puts "Lower, Bounce, Briefly"
+end
+
+def base_footwork(right)
+  puts "lift and return right their foot"
+end
+
